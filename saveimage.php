40b29a48aa555d277db55330a68d87f23c90aca8
mariachi situate <nevertalking@hotmail.com>
2017-11-29 21:38:16 +0100

    F****** php

--- saveimage.php
+++ saveimage.php
@@ -1,18 +1,22 @@
 <?php
 if (isset($GLOBALS["HTTP_RAW_POST_DATA"]))
 {
-$imgdata = $GLOBALS['HTTP_RAW_POST_DATA'];
+// Get the data
+$imageData=$GLOBALS['HTTP_RAW_POST_DATA'];
 
-    $filteredData=substr($imgdata, strpos($imgdata, ",")+1);// Remove the headers (data:,) part.
-    $data = base64_decode($filteredData);//decode data
-    $im = imagecreatefromstring($data); //create image
-    if ($im !== false) {
-        imagesavealpha($im, true);
-        imagejpeg($im, "result.jpg",9);
-    }
-    else {
-        echo 'An error occurred.';
-    }
+// Remove the headers (data:,) part.
+// A real application should use them according to needs such as to check image type
+$filteredData=substr($imageData, strpos($imageData, ",")+1);
 
-    }
+// Need to decode before saving since the data we received is already base64 encoded
+$unencodedData=base64_decode($filteredData);
+
+//echo "unencodedData".$unencodedData;
+
+// Save file. This example uses a hard coded filename for testing,
+// but a real application can specify filename in POST variable
+$fp = fopen( ‘test.jpg’, ‘wb’ );
+fwrite( $fp, $unencodedData);
+fclose( $fp );
+}
 ?>
